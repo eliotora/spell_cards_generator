f98cdd27060e66874a6b564f1e9d35d966caf3bd
eliotora <eliot.cosyn@gmail.com>
2025-06-04 22:43:53 +0200

    Finished data scrapper

--- data_scrapper.py
+++ data_scrapper.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from time import sleep
+import json
+import os
 
 class AideddNavigator():
     def __init__(self, driver):
@@ -16,6 +18,8 @@ class AideddNavigator():
 
     def multiple_select(self, checkbox_id: str, select_id: str, options: list[str]=None):
         checkbox = self.driver.find_element(By.ID, checkbox_id)
+        checkbox.click()  # Ensure the checkbox is clicked to enable multiple selection
+        checkbox.click()  # Click again to ensure it is selected
         if options is None and not checkbox.is_selected():
             checkbox.click()
         elif type(options) is list and options:
@@ -51,7 +55,10 @@ class AideddNavigator():
         self.select_level(select_name="nivMax", level=level_max)
         # Select sources
         self.multiple_select("selectAllS", "FormSource", sources)
-        self.driver.find_element(By.ID, "opt_tcoe").click()  # Select Tasha's Cauldron of Everything
+    
+        tasha_list = self.driver.find_element(By.ID, "opt_tcoe")  # Select Tasha's Cauldron of Everything
+        if not tasha_list.is_selected():
+            tasha_list.click()
 
         # Select all display options
         coche_group = self.driver.find_element(By.CLASS_NAME, "coche")
@@ -73,34 +80,151 @@ class AideddNavigator():
                 spell = {
                     "lien": cells[1].find_element(By.TAG_NAME, "a").get_attribute("href"),
                     "nom": cells[1].text,
-                    "nom_VF": cells[2].text,
-                    "nom_VO": cells[3].text,
-                    "niveau": cells[4].text,
-                    "école": cells[5].text,
-                    "temps_d'incantation": cells[6].text,
-                    "portée": cells[7].text,
-                    "composantes": cells[8].text,
-                    "concentration": True if cells[9].text else False,
-                    "rituel": True if cells[10].text else False,
-                    "description": cells[11].text,
-                    "source": cells[12].text
+                    "description_short": cells[11].text,
+                    # "source": cells[12].text
                 }
                 spells.append(spell)
         print(f"Found {len(spells)} spells.")
         return spells
 
-    def get_spell_details(self, spell_url: str):
+    def get_spell_details(self, spell, spell_url: str):
         self.driver.get(spell_url)
         sleep(2)
-        spell_details = {}
         content = self.driver.find_element(By.CLASS_NAME, "col1")
-        spell_details["nom"] = content.find_element(By.TAG_NAME, "h1").text
-        spell_details["nom_VO"] = content.find_element(By.XPATH, "").text
+        trad = content.find_element(By.CLASS_NAME, "trad")
 
+        spell["nom_VO"] = trad.find_element(By.TAG_NAME, "a").text
+        if len(trad.text.split("[")) > 2:
+            spell["nom_VF"] = trad.text.split("[")[2].split("]")[0].strip()
+        else:
+            spell["nom_VF"] = None
+        level_school = content.find_element(By.CLASS_NAME, "ecole").text
+        spell["niveau"] = int(level_school.split(" - ")[0].split("niveau ")[1])
+        spell["école"] = level_school.split(" - ")[1].split(" (")[0]
+        spell["rituel"] = True if "rituel" in level_school else False
+        spell["temps_d'incantation"] = content.find_element(By.CLASS_NAME, "t").text.split(": ")[1]
+        spell["portée"] = content.find_element(By.CLASS_NAME, "r").text.split(": ")[1]
+        spell["composantes"] = content.find_element(By.CLASS_NAME, "c").text.split(": ")[1].split(", ")
+        spell["durée"] = content.find_element(By.CLASS_NAME, "d").text.split(": ")[1]
+        spell["concentration"] = True if "concentration" in content.find_element(By.CLASS_NAME, "d").text else False
+        description = content.find_element(By.CLASS_NAME, "description").get_attribute("innerHTML")
+        spell["description"] = description.split("<strong><em>Aux niveaux supérieurs</em></strong>. ")[0].strip()
+        spell["à_niveau_supérieur"] = description.split("<strong><em>Aux niveaux supérieurs</em></strong>. ")[1].strip() if "<strong><em>Aux niveaux supérieurs</em></strong>. " in description else ""
+
+
+def scrap_source(source:str, driver:webdriver):
+    """
+    Scraps the spells from the given source.
+    :param source: The source to scrap.
+    :param driver: The webdriver to use.
+    """
+    navigator = AideddNavigator(driver)
+    navigator.accept_cookies()
+
+    for classe in ["Artificier", "Barde", "Clerc", "Druide", "Ensorceleur", "Magicien", "Occultiste", "Paladin", "Rôdeur"]:
+        spells = scrap_class_spell_list(classe, source, navigator)
+        if spells:
+            write_spell_list_to_json(classe, spells, f"data/{source}/spell_lists/")
+            print(f"Spells for {classe} from {source} written to JSON.")
+        else:
+            print(f"No spells found for {classe} in {source}.")
+
+    spells = scrap_spells_from_source(source, navigator)
+    if spells:
+        write_spells_to_json(spells, f"data/{source}/spells/")
+        print(f"All spells from {source} written to JSON.")
+    else:
+        print(f"No spells found in {source}.")
+    
+def scrap_class_spell_list(classe:str, source:str, navigator):
+    """
+    Scraps the spells for a given class and source.
+    :param classe: The class to scrap.
+    :param source: The source to scrap.
+    :param navigator: The navigator to use.
+    :return: A list of spells.
+    """
+    navigator.filter_by(
+        classes=[classe],
+        schools=None,
+        level_min=0,
+        level_max=9,
+        sources=[source]
+    )
+    sleep(1)
+    spells = navigator.get_all_lines()
+    return spells
+
+def scrap_spells_from_source(source:str, navigator):
+    """
+    Scraps all spells from a given source.
+    :param source: The source to scrap.
+    :param navigator: The navigator to use.
+    :return: A list of spells.
+    """
+    navigator.filter_by(
+        classes=None,
+        schools=None,
+        level_min=0,
+        level_max=9,
+        sources=[source]
+    )
+    sleep(1)
+    spells = navigator.get_all_lines()
+    
+    for spell in spells:
+        navigator.get_spell_details(spell, spell["lien"])
+    
+    return spells
+
+def write_spells_to_json(spells:list, path:str):
+    """
+    Writes the spells to a JSON file.
+    :param spells: The list of spells to write.
+    :param path: The path to the JSON file.
+    """
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    
+    print(f"Writing {len(spells)} spells to JSON in {path}")
+
+    for spell in spells:
+        spell_to_write = {k: v for k, v in spell.items() if k not in ["lien"]}
+        spell_name = spell_to_write["nom"].replace(" ", "_").replace("/", "_")
+        spell_path = os.path.join(path, f"{spell_name}.json")
+        with open(spell_path, 'w', encoding='utf-8') as f:
+            json.dump(spell_to_write, f, ensure_ascii=False, indent=4)
+
+def write_spell_list_to_json(classe:str, spells:list, path:str):
+    """
+    Writes the spells of a class to a JSON file.
+    :param classe: The class name.
+    :param spells: The list of spells to write.
+    :param path: The path to the JSON file.
+    """
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    
+    data = {
+        "classe": classe,
+        "sorts": [
+            spell["nom"] for spell in spells if "nom" in spell
+        ]
+    }
+
+    print(f"Writing {len(data['sorts'])} spells for {classe} to JSON in {path}")
+
+    class_path = os.path.join(path, f"{classe}.json")
+    with open(class_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     driver = webdriver.Edge()
     driver.get("https://www.aidedd.org/dnd-filters/sorts.php")
+
+    scrap_source("Player´s Handbook", driver)
+
+    exit() 
 
     navigator = AideddNavigator(driver)
     navigator.accept_cookies()
@@ -111,7 +235,11 @@ if __name__ == "__main__":
         level_max=9,
         sources=["Player´s Handbook"]
     )
+    sleep(1)
     spells = navigator.get_all_lines()
-    for spell in spells[:5]:
-        print(spell)
+    for spell in spells[:13]:
+        navigator.get_spell_details(spell, spell["lien"])
+        spell_to_write = {k: v for k, v in spell.items() if k not in ["lien"]}
+        with open(f"test/{spell_to_write["nom"].replace(" ", "_").replace("/", "_")}.json", "w", encoding='utf-8') as f:
+            json.dump(spell_to_write, f, ensure_ascii=False, indent=4)
     sleep(30)
