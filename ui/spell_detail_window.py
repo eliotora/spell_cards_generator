c61ce39c3a29ba09aa95825e1a67815819665f4e
eliotora <eliot.cosyn@gmail.com>
2025-06-05 23:32:04 +0200

    fixed tables in preview

--- ui/spell_detail_window.py
+++ ui/spell_detail_window.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QScrollArea
 from PyQt6.QtCore import Qt
+import re
 
 
 class SpellDetailWindow(QWidget):
@@ -67,10 +68,13 @@ class SpellDetailWindow(QWidget):
         self.d.setWordWrap(True)
         self.content_layout.addWidget(self.d)
 
-        self.description = QLabel(f"{spell.get('description', 'Aucune description disponible.')}")
+        description_text = spell.get("description", "Aucune description disponible.")
+        description_text = self.inbed_table_style(description_text)
+        self.description = QLabel(description_text)
         self.description.setProperty("class", "description")
         self.description.setWordWrap(True)
         self.content_layout.addWidget(self.description)
+
 
         if spell.get("à_niveau_supérieur", "") != "" and spell.get("à_niveau_supérieur", "") is not None:
             self.niveau_sup = QLabel(f"<strong><em>À niveau supérieur:</em></strong> {spell['à_niveau_supérieur']}")
@@ -113,3 +117,30 @@ class SpellDetailWindow(QWidget):
             widget = self.content_layout.itemAt(i).widget()
             if widget:
                 widget.setStyleSheet(stylesheet)
+
+    def inbed_table_style(self, description):
+        """Applies a custom style to tables in the description."""
+        if "<table" in description:
+            description = description.replace("<table", "<table style='margin:6px 0 0 0; border-spacing:0; font-family:arial, sans-serif; font-size:17px; padding:0; box-sizing: border-box; display:table; border-collapse: separate; text-indent: initial; unicode-bidi: isolate; border-color: gray; text-align: justify; padding: 6 0 6 0; line-height: 1.5; overflow-y: scroll;'")
+            description = description.replace("<tbody", "tbody style=margin:0; padding:0; box-sizing: border-box; displace: table-row-group; vertical-align: middle; unicode-bidi: isolate; border-color: inherit; margin-top: 6: border-spacing: 0; font-family: arial, sans-serif; font-size: 17px; border-collapse: separate; text-indent: initial; text-align: justify; padding: 6 0 6 0; line-height: 1.5; overflow-y: scroll;'")
+            tr_style = "margin:0; padding:0; box-sizing: border-box; display: table-row; vertical-align: inherit; unicode-bidi: isolate; border-color: inherit; margin-top: 6px; border-spacing: 0; font-family: arial, sans-serif; font-size: 17px; border-collapse: separate; text-indent: initial; text-align: justify; padding: 6px 0 6px 0; line-height: 1.5; overflow-y: scroll;"
+            description = description.replace('<th class="center"', "<th style='min-width: 45px; padding: 2 4 2 4; text-align: center; font-weight: bold; vertical-align: bottom; margin: 0; box-sizing: border-box; display: table-cell; unicode-bidi: isolate; border-spacing: 0; font-family: arial; sans-serif; font-size: 17px; border-collapse: separate; text-indent: initial; padding: 6 0 6 0; line-height: 1.5; overflow-y: scroll;'")
+            description = description.replace('<th>', "<th style='min-width: 45px; padding: 2 4 2 4; text-align: left; font-weight: bold; vertical-align: bottom; margin: 0; box-sizing: border-box; display: table-cell; unicode-bidi: isolate; border-spacing: 0; font-family: arial; sans-serif; font-size: 17px; border-collapse: separate; text-indent: initial; padding: 6 0 6 0; line-height: 1.5; overflow-y: scroll;'>")
+            description = description.replace('<td class="center"', "<td style='min-width: 45px; padding: 2 4 2 4; text-align: center; vertical-align: top; margin: 0; box-sizing: border-box; display: table-cell; unicode-bidi: isolate; border-spacing: 0; font-family: arial, sans-serif; font-size: 17px; border-collapse: separate; text-indent: initial; padding: 6 0 6 0; line-height: 1.5; overflow-y: scroll;'")
+            description = description.replace('<td>', "<td style='min-width: 45px; padding: 2 4 2 4; text-align: left; vertical-align: top; margin: 0; box-sizing: border-box; display: table-cell; unicode-bidi: isolate; border-spacing: 0; font-family: arial, sans-serif; font-size: 17px; border-collapse: separate; text-indent: initial; padding: 6 0 6 0; line-height: 1.5; overflow-y: scroll;'>")
+            
+            # Trouver tous les <tr ...> et ajouter un style selon l'index
+            def tr_replacer(match):
+                idx = tr_replacer.counter
+                tr_replacer.counter += 1
+                style = tr_style + " background-color: #D6d0e0;" if idx % 2 == 1 else tr_style
+                if style:
+                    return f"<tr style='{style}'"
+                else:
+                    return "<tr"
+            tr_replacer.counter = 0
+
+            # Remplacer chaque <tr> (avec ou sans attributs)
+            description = re.sub("<tr", tr_replacer, description)
+            description = description.replace("</table>", "</table><br>")
+        return description
