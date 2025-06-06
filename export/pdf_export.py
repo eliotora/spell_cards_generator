25c0bc06ba355a027e7251730c4b8052237f4049
Eliot Cosyn <eliot.cosyn@hubruxelles.be>
2025-06-06 09:53:19 +0200

    Fixed pdf export

--- export/pdf_export.py
+++ export/pdf_export.py
@@ -12,17 +12,17 @@ def html_to_pdf(html_path, output_filename):
         browser.close()
 
 
-def exporter_pdf(spells, path, mode='rules'):
+def exporter_pdf(spells, path, mode='rules', show_source=False, show_VO_name=False):
     if not spells:
         return
-    
+
     i = random.randint(0, 1000000)
     html_path = path.replace('.pdf', f'_{i}.html')
     while os.path.exists(html_path):
         i = random.randint(0, 1000000)
         html_path = path.replace('.pdf', f'_{i}.html')
 
-    html_export(spells, html_path, mode)
+    html_export(spells, html_path, mode, show_source=show_source, show_VO_name=show_VO_name)
 
     html_to_pdf(html_path, path)
 
